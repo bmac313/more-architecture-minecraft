d8675f501e45f0c7b5427e3f46584a7d00bca50d
Brendan <mcb0193@comcast.net>
2017-10-07 10:59:07 -0500

    Create class ModItems

---
+++ src/main/java/com/github/architecture/helpers/ModItems.java
@@ -0,0 +1,11 @@
+package com.github.architecture.helpers;
+
+import com.github.architecture.block.GrassSlab;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModItems {
+
+    @GameRegistry.ObjectHolder("architecture:grass_slab")
+    public static GrassSlab grassSlab;
+
+}
