0174010f648b5a95ee439e5679e51b881d756866
Brendan <mcb0193@comcast.net>
2017-10-14 12:21:02 -0500

    Update ModBlocks and ModItems

--- src/main/java/com/github/architecture/helpers/ModBlocks.java
+++ src/main/java/com/github/architecture/helpers/ModBlocks.java
@@ -1,15 +1,21 @@
 package com.github.architecture.helpers;
 
+import com.github.architecture.block.ClayBrick;
 import com.github.architecture.block.GrassSlab;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ModBlocks {
 
     @GameRegistry.ObjectHolder("architecture:grass_slab")
     public static GrassSlab grassSlab;
+    public static ClayBrick clayBrick;
 
+    @SideOnly(Side.CLIENT)
     public static void initModels() {
         grassSlab.initModel();
+        clayBrick.initModel();
     }
 
 }
--- src/main/java/com/github/architecture/helpers/ModItems.java
+++ src/main/java/com/github/architecture/helpers/ModItems.java
@@ -1,5 +1,6 @@
 package com.github.architecture.helpers;
 
+import com.github.architecture.block.ClayBrick;
 import com.github.architecture.block.GrassSlab;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -9,10 +10,12 @@ public class ModItems {
 
     @GameRegistry.ObjectHolder("architecture:grass_slab")
     public static GrassSlab grassSlab;
+    public static ClayBrick clayBrick;
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         grassSlab.initModel();
+        clayBrick.initModel();
     }
 
 }
