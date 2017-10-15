78891606cf171838b703aa88996edbd94a01f884
Brendan <mcb0193@comcast.net>
2017-10-15 15:47:04 -0500

    Create superclass for Painted Wood blocks

---
+++ src/main/java/com/github/architecture/block/wood/PaintedWoodPlanks.java
@@ -0,0 +1,12 @@
+package com.github.architecture.block.wood;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class PaintedWoodPlanks extends Block {
+
+    public PaintedWoodPlanks() {
+        super(Material.WOOD);
+    }
+
+}
